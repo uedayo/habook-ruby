7c36fe1ae794a99c52b927ab80d1cb92b8983d3e
Yoshinori Ueda <uedayo@gmail.com>
2013-06-21 16:18:27 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Habook::Application.config.secret_token = '5c1fd971aed61f9a3833ae3b152446ba1de85ddcac67169165227e952eeba09bbf09303ca4cadd4f73d00d1ad17f1d99cef7ef4e234ba74f6297b756a16d78ef'
