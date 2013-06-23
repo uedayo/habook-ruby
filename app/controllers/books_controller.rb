c02b472484cf45373573c8aa29493b90fe92f6b1
Yoshinori Ueda <uedayo@gmail.com>
2013-06-23 17:00:35 +0900

    add view

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,24 @@
 class BooksController < ApplicationController
+
+  def index
+    @books = Book.all(:order => "updatedAt DESC")
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(params[:post])
+    if @book.save
+      redirect_to books_path
+    else
+      render action: 'new'
+    end
+  end
+
 end
