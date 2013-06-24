9a971c7e94ece4b839388beb73159e53ec2fb0ac
Yoshinori Ueda <uedayo@gmail.com>
2013-06-24 10:56:31 +0900

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# encoding: UTF-8
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +7,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Book
+Book.delete_all
+
+Book.create(
+    title: 'RailsによるアジャイルWebアプリケーション開発 第4版',
+    publisher: 'オーム社',
+    smallImage: 'http://ec2.images-amazon.com/images/I/51Gibg-iYTL._BO2,204,203,200_PIsitb-sticker-arrow-click,TopRight,35,-76_AA300_SH20_OU09_.jpg',
+    mediumImage: 'http://ec2.images-amazon.com/images/I/51Gibg-iYTL._BO2,204,203,200_PIsitb-sticker-arrow-click,TopRight,35,-76_AA300_SH20_OU09_.jpg',
+    listPrice: 3360,
+    isbn: '4274068668',
+    detailPageUrl: 'http://www.amazon.co.jp/dp/4274068668',
+    number: 1,
+    status: 0,
+    publishedAt: '2011-12-01',
+    createdAt: '2013-06-21 11:55:00.373973',
+    updatedAt: '2013-06-21 11:55:00.373973'
+)
