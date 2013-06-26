4852cd929a2615bd58f8fe1912a8d82e388e795d
Yoshinori Ueda <uedayo@gmail.com>
2013-06-26 19:51:19 +0900

    modify layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 class ApplicationController < ActionController::Base
   protect_from_forgery
 end
@@ -5,3 +7,25 @@ end
 $twitter_image_width = 128
 $book_image_width = 100
 $book_image_large_width = 200
+
+$app_title = "ほんどこ？"
+
+$book_title = "タイトル"
+$book_author = "著者"
+$book_manufacturer = "出版社"
+$book_isbn = "ISBN"
+$book_status = "状態"
+$book_able = "貸出可能"
+$book_unable = "貸出中"
+$book_all = "本一覧"
+
+$user_screen_name = "Twitter ID"
+$user_name = "名前"
+$user_all = "ユーザ一覧"
+
+$action_lend = "借りる"
+$action_return = "返す"
+
+$button_submit = "送信"
+$button_edit = "編集"
+$button_delete = "削除"
